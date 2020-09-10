05eb2c024e84b9e2175a78bf5fdf7b2c7eeee996
BukunmiOla <ebukunmiola@gmail.com>
2020-09-10 06:04:48 +0100

    feat: SystemUI manager added to enable fullscreen

--- app/src/main/java/com/example/gadsleaderboard/ui/SystemUIManager.java
+++ app/src/main/java/com/example/gadsleaderboard/ui/SystemUIManager.java
@@ -4,13 +4,13 @@ import android.view.View;
 
 public class SystemUIManager {
     
-    private void hideSystemUI() {
+    public void hideSystemUI(View view) {
         // Enables regular immersive mode.
         // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
         // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        View decorView = getWindow().getDecorView();
+        View decorView = view;
         decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         // Set the content to appear under the system bars so that the
                         // content doesn't resize when the system bars hide and show.
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -23,8 +23,8 @@ public class SystemUIManager {
 
     // Shows the system bars by removing all the flags
 // except for the ones that make the content appear under the system bars.
-    private void showSystemUI() {
-        View decorView = getWindow().getDecorView();
+    public void showSystemUI(View view) {
+        View decorView = view;
         decorView.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
